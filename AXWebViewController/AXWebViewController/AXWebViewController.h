6b1ff9d2cbfc7e81bda8d28de80e4ee3cc7cc0cd
aixing-hb <aixing@hydsoft.com>
2017-10-18 09:56:51 +0800

    Updated pod settings.

--- AXWebViewController/AXWebViewController/AXWebViewController.h
+++ AXWebViewController/AXWebViewController/AXWebViewController.h
@@ -113,7 +113,7 @@ AX_WEB_VIEW_CONTROLLER_AVAILABLITY;
 
 #if AX_WEB_VIEW_CONTROLLER_USING_WEBKIT
 typedef NSURLSessionAuthChallengeDisposition (^WKWebViewDidReceiveAuthenticationChallengeHandler)(WKWebView *webView, NSURLAuthenticationChallenge *challenge, NSURLCredential * _Nullable __autoreleasing * _Nullable credential);
-
+API_AVAILABLE(ios(8.0))
 @interface AXWebViewController : UIViewController <WKUIDelegate, WKNavigationDelegate>
 {
     @protected
@@ -121,6 +121,7 @@ typedef NSURLSessionAuthChallengeDisposition (^WKWebViewDidReceiveAuthentication
     NSURL *_URL;
 }
 #else
+API_AVAILABLE(ios(7.0))
 @interface AXWebViewController : UIViewController <UIWebViewDelegate>
 {
 @protected
